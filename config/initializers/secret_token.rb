f2cc0d833d1c5ad88e8caa8a1ce3de28326ee8bf
Jacky Wu <jackywu07@berkeley.edu>
2011-09-10 17:13:53 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dengue::Application.config.secret_token = 'bcde7886d24763531679077b47da15523df11f8f43933afda9d544436439915bbaba085b3ae21dc69ed83231b9e0825c20f7cfbf5f4ebe7d0b27c571312a4660'
