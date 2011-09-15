b1936372161209d0a45e893f041bee0fe0bfb54d
Hui Peng Hu <woohp135@gmail.com>
2011-09-15 14:54:37 -0700

    use cookies instead of sessions for authentication. added the remember_me option using cookies. changed to use alerts instead of warnings in flashes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,19 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_username(params[:username])
     if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
+      if params[:remember_me]
+        cookies.permanent[:auth_token] = user.auth_token
+      else
+        cookies[:auth_token] = user.auth_token
+      end
       redirect_to root_url, :notice => "Signed in!"
     else
-      flash[:warning] = "Invalid username or password."
-      redirect_to new_session_path
+      redirect_to new_session_path, :alert => "Invalid username or password."
     end
   end
 
   def destroy
-    session[:user_id] = nil
+    cookies.delete(:auth_token)
     redirect_to root_url, :notice => "Signed out!"
   end
 
