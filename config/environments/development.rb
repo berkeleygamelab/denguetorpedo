9c973e37242554f07af599bfdcbd4525dda480ed
Hui Peng Hu <woohp135@gmail.com>
2011-09-23 21:27:13 -0700

    uncommented config.action_mailer.default_url_options and added it for production.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,18 +28,17 @@ Dengue::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  #config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
   
   config.action_mailer.delivery_method = :smtp
    # Gmail SMTP server setup
-   config.action_mailer.smtp_settings = {
-         :address => "smtp.gmail.com",
-         :enable_starttls_auto => true,
-         :port => 587,
-         :domain => 'reportdengue@gmail.com',
-         :authentication => :plain,
-         :user_name => 'reportdengue',
-         :password => 'dengue@!$'
-   }
-
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :enable_starttls_auto => true,
+    :port => 587,
+    :domain => 'reportdengue@gmail.com',
+    :authentication => :plain,
+    :user_name => 'reportdengue',
+    :password => 'dengue@!$'
+  }
 end
