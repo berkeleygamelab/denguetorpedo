51aa68ecb6945ee00d854b90f374d41c205a9a16
Hui Peng Hu <woohp135@gmail.com>
2012-02-05 21:11:22 -0800

    put a filter in the verification controller to check that the user is a verifier

--- app/controllers/verifications_controller.rb
+++ app/controllers/verifications_controller.rb
@@ -1,4 +1,14 @@
 class VerificationsController < ApplicationController
+
+  before_filter :require_login
+  before_filter :check_is_verifier
+
+  private
+  def check_is_verifier
+    head :forbidden and return unless @current_user.is_verifier
+  end
+
+  public
   def index
   end
 
