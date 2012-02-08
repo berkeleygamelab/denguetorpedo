cba6ae918dc67ee06be522dcec7bc7579a074231
Hui Peng Hu <woohp135@gmail.com>
2012-02-08 02:30:34 -0800

    Figured out how to get the top neighborhoods and its houses!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def index
     # NEIGHBORHOOD MODEL HAS NOT BEEN IMPLEMENTED AND INTEGRATED WITH THE APP YET!!!!
-    #@neighborhoods = Neighborhood.top_five
-    #@houses_list = @neighborhoods.collect { |n| n.houses }
+
+    neighborhoods = Location.top_neighborhoods(5)
+    houses_list = House.all_in_neighborhood(neighborhoods)
     #@houses_maps = @houses_list.collect do |houses| 
     #  houses.collect{ |h| h.location }.to_gmaps4rails
     #end
--- app/models/house.rb
+++ app/models/house.rb
@@ -8,8 +8,11 @@ class House < ActiveRecord::Base
   belongs_to :location
 
   def points
-    # TODO: change this to use a SQL query because this is probably not that efficient
-    members.map {|m| m.points}.sum
+    members.sum(:points)
+  end
+
+  def self.all_in_neighborhoods(neighborhoods)
+    House.joins(:location).where(:locations => {:neighborhood => neighborhoods})
   end
 
   def reports
