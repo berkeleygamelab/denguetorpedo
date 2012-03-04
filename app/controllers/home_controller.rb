009246aa2131fb88ba9c33c4b0fd6668f3ec881f
Jacky Wu <jackywu07@berkeley.edu>
2012-03-04 12:14:54 -0800

    made the home and non_home nav bar. and fix the alignment issue for the house page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   def index
     @user = User.new
     @all_neighborhoods = Location.top_neighborhoods
-
+    @is_home = "home_nav"
+    
     if params[:neighborhood].nil?
       @houses = House.all_in_neighborhoods(@all_neighborhoods.first)
     else
