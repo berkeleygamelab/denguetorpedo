3bc4d68c546163e3f977090e5bfd005e6ca09c06
Jacky Wu <jackywu07@berkeley.edu>
2012-03-06 23:17:58 -0800

    home page implementation completed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,9 +6,14 @@ class HomeController < ApplicationController
     
     if params[:neighborhood].nil?
       @houses = House.all_in_neighborhoods(@all_neighborhoods.first)
+      @selected_neighborhood = @all_neighborhoods.first
     else
       @houses = House.all_in_neighborhoods(params[:neighborhood])
+      @selected_neighborhood = params[:neighborhood]
     end
+    @total_reports_in_neighborhood = Report.joins(:location).where(:locations => {:neighborhood => @selected_neighborhood}).count
+    
+    #Report.count_by_sql("SELECT COUNT(*) FROM reports, users, houses, locations WHERE (reports.reporter_id = users.id OR reports.claimer_id = users.id OR reports.eliminator_id = users.id) AND users.house_id = houses.id AND houses.location_id = locations.id AND locations.neighborhood = 'South Side'")
   end
   
 end
