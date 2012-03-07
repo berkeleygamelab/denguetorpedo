d87d0cdb48ee1f4130737aacebfcc8457c9ac659
Hui Peng Hu <woohp135@gmail.com>
2012-03-06 21:21:10 -0800

    added validations to models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,16 @@
 class Event < ActiveRecord::Base
   belongs_to :creator, :class_name => "User"
   has_many :comments, :class_name => "EventComment"
+
+  validates :creator_id, :presence => true
+  validates :description, :presence => true
+  validates :category, :presence => true
+  validates :praised, :presence => true
+
+  after_initialize :default_values
+
+  private
+  def default_values
+    self.praised ||= 0
+  end
 end
