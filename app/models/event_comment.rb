d64ff6e4fe73f70e3313a07de7c11575381576b4
Hui Peng Hu <woohp135@gmail.com>
2012-05-27 14:42:02 -0700

    enumerize fixe for event comment

--- app/models/event_comment.rb
+++ app/models/event_comment.rb
@@ -7,6 +7,6 @@ class EventComment < ActiveRecord::Base
   validates :event_id, :presence => true
 
   after_create do |event_comment|
-    Feed.create_from_object(event_comment, event_comment.user_id, "event_comment")
+    Feed.create_from_object(event_comment, event_comment.user_id, :event_comment)
   end
 end
