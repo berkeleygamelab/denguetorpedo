d599b0a88a686437ad7e5d08ff1b32a18c8f87d2
Jacky Wu <jackywu07@berkeley.edu>
2012-06-02 02:47:54 -0700

    fix migration and fix login section

--- db/migrate/20120328061629_remove_before_and_after_photo_from_reports.rb
+++ db/migrate/20120328061629_remove_before_and_after_photo_from_reports.rb
@@ -1,38 +1,4 @@
 class RemoveBeforeAndAfterPhotoFromReports < ActiveRecord::Migration
-<<<<<<< HEAD
   def change
-    #remove_column :reports, :before_photo_file_name, :string
-    #remove_column :reports, :before_photo_content_type, :string
-    #remove_column :reports, :before_photo_file_size, :integer
-    #remove_column :reports, :before_photo_updated_at, :datetime
-    
-    #remove_column :reports, :after_photo_file_name, :string
-    #remove_column :reports, :after_photo_content_type, :string
-    #remove_column :reports, :after_photo_file_size, :integer
-    #remove_column :reports, :after_photo_updated_at, :datetime
-=======
-  def up
-    remove_column :reports, :before_photo_file_name
-    remove_column :reports, :before_photo_content_type
-    remove_column :reports, :before_photo_file_size
-    remove_column :reports, :before_photo_updated_at
-    
-    remove_column :reports, :after_photo_file_name
-    remove_column :reports, :after_photo_content_type
-    remove_column :reports, :after_photo_file_size
-    remove_column :reports, :after_photo_updated_at
-  end
-  
-  def down
-    add_column :reports, :before_photo_file_name, :string
-    add_column :reports, :before_photo_content_type, :string
-    add_column :reports, :before_photo_file_size, :integer
-    add_column :reports, :before_photo_updated_at, :datetime
-    add
-    add_column :reports, :after_photo_file_name, :string
-    add_column :reports, :after_photo_content_type, :string
-    add_column :reports, :after_photo_file_size, :integer
-    add_column :reports, :after_photo_updated_at, :datetime
->>>>>>> home_slider_rework_dev
   end
 end
