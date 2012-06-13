90e1aff26c18e094231e8ef25fdd9e7efaa4ead9
Ian Norris <ian_norris@berkeley.edu>
2012-06-12 20:30:35 -0700

    style change and updates to neighborhood

--- app/controllers/neighborhoods_controller.rb
+++ app/controllers/neighborhoods_controller.rb
@@ -2,5 +2,9 @@ class NeighborhoodsController < ApplicationController
   def show
     @neighborhood = Neighborhood.find(params[:id])
     head :not_found and return if @neighborhood.nil?
+    
+    
+    @houses = @neighborhood.houses.limit(10)
+    @total_reports_in_neighborhood = @neighborhood.reports.count
   end
 end
