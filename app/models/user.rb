6f227dcf7522c73dce551c8e742a939329feb5ae
Hui Peng Hu <woohp135@gmail.com>
2012-07-09 00:38:22 -0700

    validate user email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ActiveRecord::Base
   validates :password, :length => { :minimum => 4, :message => "should contain at least 4 characters" }, :if => "id.nil? || password"
   validates :points, :numericality => { :only_integer => true }
   validates :phone_number, :numericality => true, :length => { :minimum => 10, :maximum => 20 }, :allow_nil => true
+  validates :email, :format => { :with => EMAIL_REGEX }, :allow_nil => true
 #  validates :is_fully_registered, :presence => true
 #  validates :is_community_coordinator, :presence => true
 #  validates :is_community_coordinator, :uniquness => { :scope => ??? } TODO: only want one coordinator per community
