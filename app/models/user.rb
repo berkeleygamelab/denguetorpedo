024591d0b0f83b809850ce0685422432115ae8e8
Hui Peng Hu <woohp135@gmail.com>
2012-07-09 00:29:29 -0700

    fixed users password validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   # validates
   validates :username, :uniqueness => true
   validates :username, :format => { :with => USERNAME_REGEX, :message => "should only contain letters, numbers, or .-+_@, and have between 5-15 characters" }
-  validates :password, :length => { :minimum => 4, :message => "should contain at least 4 characters" }, :if => "password_digest.nil?"
+  validates :password, :length => { :minimum => 4, :message => "should contain at least 4 characters" }, :if => "id.nil? || password"
   validates :points, :numericality => { :only_integer => true }
 #  validates :is_fully_registered, :presence => true
 #  validates :is_community_coordinator, :presence => true
