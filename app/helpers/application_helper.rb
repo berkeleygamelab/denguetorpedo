7ef1adcd1295663311b10ac01f40137c5c1dd36d
Jacky Wu <jackywu07@berkeley.edu>
2012-07-16 23:35:40 -0700

    getting SMS reader ready for the demo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   
   def self.temp_password_generator
-    char_bank = (('a'..'z').to_a + ('0'..'9').to_a + ('A'..'Z').to_a) - %w(i o 0 1 l 0)
+    char_bank = ('0'..'9').to_a
     char_bank.shuffle.shuffle.shuffle!
     (1..8).collect{|a| char_bank[rand(char_bank.size)] }.join
   end
