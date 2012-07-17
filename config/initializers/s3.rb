f8847f657ad144043edf1872ecda71cf6186887b
Hui Peng Hu <woohp135@gmail.com>
2012-07-17 01:49:00 -0700

    s3 credentials fix.

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  S3_CREDENTIALS = { access_key_id: ENV['AWS_ACCESS_KEY'], secret_access_key: ENV['SECRET_ACCESS_KEY'], bucket: ENV['S3_BUCKET_NAME'] }
+  S3_CREDENTIALS = { access_key_id: ENV['AWS_ACCESS_KEY_ID'], secret_access_key: ENV['SECRET_ACCESS_KEY'], bucket: ENV['S3_BUCKET_NAME'] }
 else
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
 end
