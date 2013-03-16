b04e433237e3fa234309f3f6c25ce7a237b676b0
lichen han <thisislichen@berkeley.edu>
2013-03-16 15:45:56 -0700

    fixies

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -9,6 +9,15 @@ class HousesController < ApplicationController
     head :not_found and return if @house.nil?
     
     @house_reports_gmap_json = @house.reports.map {|report| report.location}.to_gmaps4rails
+    @map_options =  {"center_latitude" => @house.location.latitude, 
+                       "center_longitude" => @house.location.longitude,
+                       "detect_location" => false,
+                       "center_on_user" => false,
+                       "auto_adjust" => false,
+                       "auto_zoom" => true,
+                       "zoom" => 14,
+                       "id" => "reports_map"
+                 }
     @post = Post.new
 
     #if @house.posts.exists?
