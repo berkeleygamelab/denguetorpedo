efed9262020e05e1e8cd1aa0df89fdee0781ce36
Jacky Wu <jackywu07@berkeley.edu>
2013-04-20 15:12:23 -0700

    change to unicode @ sign \u0040

--- app/controllers/sms_gateway_controller.rb
+++ app/controllers/sms_gateway_controller.rb
@@ -23,7 +23,7 @@ class SmsGatewayController < ApplicationController
     logger.info "user info = " + user.inspect
 
     if user
-      parsed_result = text.scan(/^(.+)@(.+)$/)
+      parsed_result = text.scan(/^(.+)\u0040(.+)$/)
       
       logger.info "parsed result is = " + parsed_result.to_s
       
