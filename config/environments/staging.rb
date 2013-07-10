8d3e757c0443477a3ddfc5297d05f2d0cf6ca213
Hara Kang <hara0115@gmail.com>
2013-07-10 02:59:54 -0700

    hope it works

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -85,7 +85,7 @@ Dengue::Application.configure do
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   } 
    
