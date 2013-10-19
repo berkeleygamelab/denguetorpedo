2f3fde2a6daded1010417b814679ba8a8e8ec2e1
Hara Kang <hara0115@gmail.com>
2013-10-19 11:41:35 +0900

    Changed counter setting

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,4 +2,6 @@ class Feedback < ActiveRecord::Base
   attr_accessible :email, :message, :name, :title
   validates :email, presence: true
   validates :title, presence: true
+  validates :name, presence: true
+  validates :message, presence: true
 end
